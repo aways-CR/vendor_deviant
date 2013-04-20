db6de3ef8e4abbb8b16262f709c0036c3f6b7919
Muhammed Nazim <mnm9994u@gmail.com>
2013-04-21 01:10:12 +0200

    Fix Up New Sounds [2/3]
    Change-Id: I5aa33fa1b88805df9784195deacb48d2cac5acc8
    
    Conflicts:
    	config/common.mk

--- config/common.mk
+++ config/common.mk
@@ -32,50 +32,7 @@ PRODUCT_COPY_FILES += \
 # Audio Config for DSPManager
 PRODUCT_COPY_FILES += \
     vendor/deviant/prebuilt/common/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf
-
-# Bring in media files
-PRODUCT_COPY_FILES +=  \
-    vendor/deviant/prebuilt/common/media/audio/alarms/Alarm_Classic.ogg:system/media/audio/alarms/Alarm_Classic.ogg \
-    vendor/deviant/prebuilt/common/media/audio/alarms/Alarm_Rooster_02.ogg:system/media/audio/alarms/Alarm_Rooster_02.ogg \
-    vendor/deviant/prebuilt/common/media/audio/alarms/Cesium.ogg:system/media/audio/alarms/Cesium.ogg \
-    vendor/deviant/prebuilt/common/media/audio/alarms/Alarm_Beep_02.ogg:system/media/audio/alarms/Alarm_Beep_02.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/NewPlayer.ogg:system/media/audio/ringtones/NewPlayer.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/Ring_Digital_02.ogg:system/media/audio/ringtones/Ring_Digital.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/Playa.ogg:system/media/audio/ringtones/Playa.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/Pyxis.ogg:system/media/audio/ringtones/Pyxis.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/Vespa.ogg:system/media/audio/ringtones/Vespa.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/hydra.ogg:system/media/audio/ringtones/hydra.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/Aquila.ogg:system/media/audio/ringtones/Aquila.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/World.ogg:system/media/audio/ringtones/World.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/InsertCoin.ogg:system/media/audio/ringtones/InsertCoin.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ringtones/Carina.ogg:system/media/audio/ringtones/Carina.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/Antares.ogg:system/media/audio/notifications/Antares.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/Doink.ogg:system/media/audio/notifications/Doink.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/arcturus.ogg:system/media/audio/notifications/arcturus.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/Plastic_Pipe.ogg:system/media/audio/notifications/Plastic_Pipe.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/Procyon.ogg:system/media/audio/notifications/Procyon.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/pixiedust.ogg:system/media/audio/notifications/pixiedust.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/DontPanic.ogg:system/media/audio/notifications/DontPanic.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/Palladium.ogg:system/media/audio/notifications/Palladium.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/tweeters.ogg:system/media/audio/notifications/tweeters.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/Cricket.ogg:system/media/audio/notifications/Cricket.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/Altair.ogg:system/media/audio/notifications/Altair.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/Helium.ogg:system/media/audio/notifications/Helium.ogg \
-    vendor/deviant/prebuilt/common/media/audio/notifications/Aldebaran.ogg:system/media/audio/notifications/Aldebaran.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/Undock.ogg:system/media/audio/ui/Undock.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/Dock.ogg:system/media/audio/ui/Dock.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/Lock.ogg:system/media/audio/ui/Lock.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
-    vendor/deviant/prebuilt/common/media/audio/ui/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg
-#LOCAL deviant CHANGES  - END
+#LOCAL SLIM CHANGES  - END
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
